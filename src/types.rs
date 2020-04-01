4f1870676fcc0b64014ba6660e32b50785e010db
prataprc <prataprc@gmail.com>
2020-04-01 19:16:44 +0530

    types: derive JsonSerialize for types.

--- src/types.rs
+++ src/types.rs
@@ -144,12 +144,11 @@ impl Durable<Json> for Commodity {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, JsonSerialize)]
 pub struct Company {
-    // required fields
     name: String,
+    #[json(to_string)]
     created: chrono::DateTime<chrono::Utc>,
-    // optional fields
     tags: Vec<String>,
     note: String,
 }
@@ -211,36 +210,20 @@ impl Durable<Json> for Company {
     }
 
     fn encode(&self) -> Result<Json> {
-        let tags: Vec<Json> = native_to_json_string_array!(self.tags.clone());
-
-        let value = Json::Object(vec![
-            Property::new("name", Json::String(self.name.clone())),
-            Property::new("created", Json::String(self.created.to_string())),
-            Property::new("tags", Json::Array(tags)),
-            Property::new("note", Json::String(self.note.clone())),
-        ]);
-
-        Ok(value)
+        Ok(err_at!(ConvertFail, self.clone().try_into())?)
     }
 
     fn decode(&mut self, from: &str) -> Result<()> {
-        let value: Json = err_at!(InvalidJson, from.parse())?;
-
-        self.name = json_to_native_string!(value, "/name", "company-name")?;
-        self.created = {
-            let created = json_to_native_string!(value, "/created", "company-created")?;
-            err_at!(InvalidJson, created.parse())?
-        };
-        self.tags = json_to_native_string_array!(value, "/tags", "company-tags")?;
-        self.note = json_to_native_string!(value, "/note", "company-note")?;
-
+        let jval: Json = err_at!(InvalidJson, from.parse())?;
+        *self = err_at!(InvalidJson, jval.try_into())?;
         Ok(())
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, JsonSerialize)]
 pub struct Ledger {
     name: String,
+    #[json(to_string)]
     created: chrono::DateTime<chrono::Utc>,
     tags: Vec<String>,
     note: String,
@@ -303,49 +286,33 @@ impl Durable<Json> for Ledger {
     }
 
     fn encode(&self) -> Result<Json> {
-        let tags: Vec<Json> = native_to_json_string_array!(self.tags.clone());
-
-        let value = Json::Object(vec![
-            Property::new("name", Json::String(self.name.clone())),
-            Property::new("created", Json::String(self.created.to_string())),
-            Property::new("tags", Json::Array(tags)),
-            Property::new("note", Json::String(self.note.clone())),
-        ]);
-
-        Ok(value)
+        Ok(err_at!(ConvertFail, self.clone().try_into())?)
     }
 
     fn decode(&mut self, from: &str) -> Result<()> {
-        let value: Json = err_at!(InvalidJson, from.parse())?;
-
-        self.name = json_to_native_string!(value, "/name", "ledger-name")?;
-        self.created = {
-            let created = json_to_native_string!(value, "/created", "ledger-created")?;
-            err_at!(InvalidJson, created.parse())?
-        };
-        self.tags = json_to_native_string_array!(value, "/tags", "ledger-tags")?;
-        self.note = json_to_native_string!(value, "/note", "ledger-note")?;
-
+        let jval: Json = err_at!(InvalidJson, from.parse())?;
+        *self = err_at!(InvalidJson, jval.try_into())?;
         Ok(())
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, JsonSerialize)]
 pub(crate) struct Creditor {
     ledger: String,
     commodity: (String, f64),
 }
 
-#[derive(Clone)]
+#[derive(Clone, JsonSerialize)]
 pub(crate) struct Debitor {
     ledger: String,
     commodity: (String, f64),
 }
 
-#[derive(Clone)]
+#[derive(Clone, JsonSerialize)]
 pub struct Transaction {
     pub(crate) uuid: u128,
     pub(crate) payee: String,
+    #[json(to_string)]
     pub(crate) created: chrono::DateTime<chrono::Utc>,
     pub(crate) creditors: Vec<Creditor>,
     pub(crate) debitors: Vec<Debitor>,
@@ -451,95 +418,12 @@ impl Durable<Json> for Transaction {
     }
 
     fn encode(&self) -> Result<Json> {
-        let tags: Vec<Json> = native_to_json_string_array!(self.tags.clone());
-
-        let mut creditors: Vec<Json> = vec![];
-        for creditor in self.creditors.iter() {
-            let commodity = Json::Object(vec![
-                Property::new("name", Json::new(creditor.commodity.0.clone())),
-                Property::new("value", Json::new(creditor.commodity.1)),
-            ]);
-            creditors.push(Json::Object(vec![
-                Property::new("ledger", Json::String(creditor.ledger.clone())),
-                Property::new("commodity", commodity),
-            ]));
-        }
-
-        let mut debitors: Vec<Json> = vec![];
-        for debitor in self.debitors.iter() {
-            let commodity = Json::Object(vec![
-                Property::new("name", Json::new(debitor.commodity.0.clone())),
-                Property::new("value", Json::new(debitor.commodity.1)),
-            ]);
-            debitors.push(Json::Object(vec![
-                Property::new("ledger", Json::String(debitor.ledger.clone())),
-                Property::new("commodity", commodity),
-            ]));
-        }
-
-        let value = Json::Object(vec![
-            Property::new("uuid", Json::String(self.uuid.to_string())),
-            Property::new("payee", Json::String(self.payee.clone())),
-            Property::new("created", Json::String(self.created.to_string())),
-            Property::new("creditors", Json::Array(creditors)),
-            Property::new("debitors", Json::Array(debitors)),
-            Property::new("tags", Json::Array(tags)),
-            Property::new("note", Json::String(self.note.clone())),
-        ]);
-
-        Ok(value)
+        Ok(err_at!(ConvertFail, self.clone().try_into())?)
     }
 
     fn decode(&mut self, from: &str) -> Result<()> {
-        let value: Json = err_at!(InvalidJson, from.parse())?;
-
-        self.uuid = {
-            let uuid = json_to_native_string!(value, "/uuid", "transaction-uuid")?;
-            err_at!(InvalidJson, uuid.parse())?
-        };
-        self.payee = json_to_native_string!(value, "/payee", "transaction-name")?;
-        self.created = {
-            let created = json_to_native_string!(value, "/created", "transaction-created")?;
-            err_at!(InvalidJson, created.parse())?
-        };
-
-        match err_at!(InvalidJson, value.get("/creditors"))?.to_array() {
-            Some(cs) => {
-                for c in cs.into_iter() {
-                    let ledger = json_to_native_string!(c, "/ledger", "transaction-creditor")?;
-                    let name =
-                        json_to_native_string!(c, "/commodity/name", "transaction-creditor")?;
-                    let value = err_at!(InvalidJson, c.get("/commodity/value"))?
-                        .to_float()
-                        .unwrap();
-                    self.creditors.push(Creditor {
-                        ledger,
-                        commodity: (name, value),
-                    });
-                }
-            }
-            None => return Err(Error::InvalidJson(format!("transaction-creditors"))),
-        }
-        match err_at!(InvalidJson, value.get("/debitors"))?.to_array() {
-            Some(ds) => {
-                for d in ds.into_iter() {
-                    let ledger = json_to_native_string!(d, "/ledger", "transaction-creditor")?;
-                    let name =
-                        json_to_native_string!(d, "/commodity/name", "transaction-creditor")?;
-                    let value = err_at!(InvalidJson, d.get("/commodity/value"))?
-                        .to_float()
-                        .unwrap();
-                    self.debitors.push(Debitor {
-                        ledger,
-                        commodity: (name, value),
-                    });
-                }
-            }
-            None => return Err(Error::InvalidJson(format!("transaction-debitors"))),
-        }
-        self.tags = json_to_native_string_array!(value, "/tags", "transaction-tags")?;
-        self.note = json_to_native_string!(value, "/note", "transaction-note")?;
-
+        let jval: Json = err_at!(InvalidJson, from.parse())?;
+        *self = err_at!(InvalidJson, jval.try_into())?;
         Ok(())
     }
 }
